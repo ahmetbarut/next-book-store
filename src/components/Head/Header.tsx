a80609c76bcdce90c6e95ff8f18207bd1c168498
Ahmet Barut <ahmetbarut588@gmail.com>
2023-03-24 02:36:06 +0300

    init next project

---
+++ src/components/Head/Header.tsx
@@ -0,0 +1,11 @@
+import Phone from "./Phone";
+
+export default function Header() {
+    return (
+        <header className="w-full bg-[#393280] h-10">
+            <div className="flex w-11/12 mx-auto items-center h-full">
+                <Phone />
+            </div>
+        </header >
+    );
+}
---
+++ src/components/Head/Phone.tsx
@@ -0,0 +1,7 @@
+export default function Phone() {
+    return (
+        <div className="flex text-white">
+             +7 (999) 999-99-99
+        </div>
+    );
+}
